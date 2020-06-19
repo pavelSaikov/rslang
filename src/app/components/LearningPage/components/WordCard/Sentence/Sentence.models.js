5a9362b0f5df7ee3e37e202b465388384f200381
Pavel Saikov <pavsaikov@gmail.com>
2020-06-19 18:45:15 +0300

    feat: implement sentence component

---
+++ src/app/components/LearningPage/components/WordCard/Sentence/Sentence.models.js
@@ -0,0 +1,13 @@
+export const INPUT_STATE = {
+  INITIAL: 'INITIAL',
+  CORRECT: 'CORRECT',
+  MISTAKE: 'MISTAKE',
+};
+
+export const INPUT_STATE_COLOR_MAP = new Map([
+  [INPUT_STATE.INITIAL, '#438c9a'],
+  [INPUT_STATE.CORRECT, '#3cc977'],
+  [INPUT_STATE.MISTAKE, '#f45c53'],
+]);
+
+export const FONT_SIZE = 20;
