76645e22921c93b8f1b7e10a4692e9bd84ffab60
Siarhei Labuts <sergeylabut@gmail.com>
2020-06-22 16:44:18 +0300

    fix: style fix

--- src/app/components/LoginPage/LoginPage.jsx
+++ src/app/components/LoginPage/LoginPage.jsx
@@ -4,6 +4,7 @@ import { Password } from './components/Password/Password';
 import { Button } from './components/Button/Button';
 import { LinkWithDescription } from './components/LinkWithDescription/LinkWithDescription';
 import { useStyles } from './LoginPage.styles';
+import './theme/style.css';
 
 export const LoginPage = () => {
   const [emailState, setEmailState] = useState({ isValid: false, email: null });
@@ -17,13 +18,19 @@ export const LoginPage = () => {
   }, [emailState.isValid, passwordState.isValid]);
 
   return (
-    <div className={classes.wrapperFlexRow}>
-      <div className={classes.wrapperFlexColumn}>
-        <div className={classes.wrapper}>
-          <Email setEmailState={setEmailState} header="Log In" caption="Email or login"></Email>
-          <Password setPasswordState={setPasswordState} caption="Password"></Password>
-          <Button text="Log In" submit={submit}></Button>
-          <LinkWithDescription description="Don't have an account?" path="" linkCaption="Sign up"></LinkWithDescription>
+    <div className="login-page">
+      <div className={classes.wrapperFlexRow}>
+        <div className={classes.wrapperFlexColumn}>
+          <div className={classes.wrapper}>
+            <Email setEmailState={setEmailState} header="Log In" caption="Email or login"></Email>
+            <Password setPasswordState={setPasswordState} caption="Password"></Password>
+            <Button text="Log In" submit={submit}></Button>
+            <LinkWithDescription
+              description="Don't have an account?"
+              path=""
+              linkCaption="Sign up"
+            ></LinkWithDescription>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/LoginPage/LoginPage.styles.js
+++ src/app/components/LoginPage/LoginPage.styles.js
@@ -3,7 +3,7 @@ import { createUseStyles } from 'react-jss';
 export const useStyles = createUseStyles(() => {
   return {
     wrapperFlexRow: {
-      width: '100vw',
+      width: '80vw',
       display: 'flex',
       justifyContent: 'center',
     },
